c046c0ef42cbdf418c50c32ffefb07abfc635415
Eniadebisi <eniadebisi@gmail.com>
2023-03-16 13:23:12 -0400

    Setup 1 Remove excess, start layout

---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+      </footer>
+    </>
+  )
+}
