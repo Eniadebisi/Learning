7182001714d51308bd8851ea1918a351135e4cc6
Eni <Eniadebisi@gmail.com>
2023-03-18 15:33:41 -0400

    3.18

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+const country = 'USA'
+const continent = 'North America'
+let pop = 300
+
+let isIsland = false;
+const language = "English";
+
+let description = `${country} is in ${continent} and it's ${pop} million people speak ${language}`
+
+document.getElementById("country").innerHTML = country;
+document.getElementById("continent").innerHTML = continent;
+document.getElementById("pop").innerHTML = pop;
+document.getElementById("print").innerHTML = description;
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
