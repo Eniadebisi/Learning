76bc228b443130a27a55209866501a939b1c1bbb
dan35asd <dan35asd@gmail.com>
2024-01-22 17:52:00 -0500

    Delete old netlify files

--- components/Sidebar.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
-    </>
-  )
-}
